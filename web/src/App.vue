dcac7486a2984b38c1558343e37a8e1a5bc6dceb
yangjun wu <3291663687@qq.com>
2024-01-12 12:18:07 +0800

    回归？

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,42 +1,50 @@
 <template>
-  <div>
+  <!-- <div>
     <div>Bot昵称：{{bot_name}}</div>
     <div>Bot战力：{{bot_rating}}</div>
-  </div>
+  </div> -->
+
+
+  <NavBar />
+
   <router-view/>
 </template>
 
 
 <script>
-import $ from "jquery";
-import { ref } from "vue";
-
+import NavBar from '@/components/NavBar.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
 
 
 export default{
-
-
-  name:"APP",
-  setup:()=>{
-
-    let bot_name=ref("");
-    let bot_rating=ref("");
-
-    $.ajax({
-      url:"http://localhost:3000/pk/getbotinfo/",
-      type:"get",
-      success: resp=>{
-        bot_name.value=resp.name;
-        bot_rating.value=resp.rating;
-
-      }
-
-    })
-    return{
-      bot_name,bot_rating
-    } 
+  components:{
+    NavBar
   }
 }
+
+// import $ from "jquery";
+// import { ref } from "vue";
+
+// export default{
+//   name:"APP",
+//   setup:()=>{
+//     let bot_name=ref("");
+//     let bot_rating=ref("");
+
+//     $.ajax({
+//       url:"http://localhost:3000/pk/getbotinfo/",
+//       type:"get",
+//       success: resp=>{
+//         bot_name.value=resp.name;
+//         bot_rating.value=resp.rating;
+//       }
+//     })
+//     return{
+//       bot_name,bot_rating
+//     } 
+//   }
+// }
 
 
 </script>
@@ -47,8 +55,8 @@ export default{
 <style>
 
 body{
-  background-image: url("@/assets/1.png");
-  background-size:auto;
+  background-image: url("@/assets/2.png");
+  background-size:cover;  
 }
 
 
