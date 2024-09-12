e54f3c09eef5623a7b143a0ca25b01126a5565a7
wu-yangjun <110643670+YDream5@users.noreply.github.com>
2024-09-12 23:48:39 +0800

    Update BackendApplication.java
    trying gcc

--- backend/src/main/java/com/kob/backend/BackendApplication.java
+++ backend/src/main/java/com/kob/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package com.kob.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplicatgcc
 public class BackendApplication {
 
     public static void main(String[] args) {
