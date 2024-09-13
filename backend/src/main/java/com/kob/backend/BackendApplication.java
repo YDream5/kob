4365b0226d2fcd2b520af88538d0e797c499d0cb
wu-yangjun <110643670+YDream5@users.noreply.github.com>
2024-09-13 10:52:13 +0800

    Update BackendApplication.java
    still testing

--- backend/src/main/java/com/kob/backend/BackendApplication.java
+++ backend/src/main/java/com/kob/backend/BackendApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplicatgcc
+@Configuration
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, aab);
+        SpringApplication.run(BackendApplication.class, cccaab);
         ccc
     }
 
